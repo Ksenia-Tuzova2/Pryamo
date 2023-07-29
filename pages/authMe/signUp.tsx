24372eee39e02390279bb35e05eb40ac57147d85
Ksenia Tuzova <102681427+Ksenia-Tuzova2@users.noreply.github.com>
2023-07-29 11:36:00 +0300

    new details

--- pages/authMe/signUp.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SignUpContainer from '../../containers/auth/signup';
-
-export default function SignupPage() {
-    return (
-        <SignUpContainer />
-    )
-}
